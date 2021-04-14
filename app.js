4b73e0750c2989edd5c1c087429f7878668536af
Snegno <yoko1505@yandex.ru>
2021-04-15 00:24:06 +0300

    немного дописал код

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ Vue.createApp({
 	data() {
 		return {
 			//переменные
+			products: [
+			{img_src: 'img/1.png', price: 1000, about: 'Лодка надувная, зеленая.'},
+			{img_src: 'img/2.png', price: 2000, about: 'ЛЛодка надувная, желтая.'},
+			{img_src: 'img/3.png', price: 3000, about: 'Лодка с веслами.'}
+			],
+			index: ''
 		}
 	},
 	methods: {
 		//методы
-		test(){}
+		clickBuy(){
+				console.log('это клик!')
+		}
 	}
 }).mount('#app');
