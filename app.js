d5d44b80d83aefeccca2958960880f8e0e427dfd
Snegno <yoko1505@yandex.ru>
2021-04-15 18:12:29 +0300

    борьба с лишними Ли
    отображаем одну, но правильную(

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ Vue.createApp({
 			className: '',
 			button_id: '',
 			count: 1,
+			count_index: 0,
 			show: true,
+			show_in_basket: true,
 			buy_products_img: [],
 			buy_products_about: [],
 			buy_products_price: [],
@@ -22,20 +24,28 @@ Vue.createApp({
 	methods: {
 		//методы
 		clickBuy(event){
+
+			console.log(event)
+
 			//className = event.target.className;
-			button_id = event.target.id;
+			this.button_id = event.target.id;
+			console.log('button_id='+this.button_id)
 			//передаем src картинки
-			this.buy_products_img[button_id] = this.products[button_id].img_src;
+			this.buy_products_img[this.count_index] = this.products[this.button_id].img_src;
 			//передаем описание
-			this.buy_products_about[button_id] = this.products[button_id].about;
+			this.buy_products_about[this.count_index] = this.products[this.button_id].about;
 			//передаем цену
-			this.buy_products_price[button_id] = this.products[button_id].price;
-			this.buy_products_price_basket[button_id] = this.products[button_id].price;
-			//зачищаем переменную с классом
-			this.button_id = button_id;
+			this.buy_products_price[this.count_index] = this.products[this.button_id].price;
+			this.buy_products_price_basket[this.count_index] = this.products[this.button_id].price;
+		
+			this.button_id = '';
 			//this.className = '';
-			this.buy_products_count[button_id]=1;
+			this.buy_products_count[this.button_id]=1;
 			this.show = false;
+			console.log('this.buy_products_img='+this.buy_products_img);
+
+			
+			console.log('this.buy_products_img='+this.buy_products_img[0])
 		},
 		priceVolumeUp(index){
 			//увеличиваем счетчик в массиве чтобы у каждого эл-та был свой count
@@ -45,7 +55,7 @@ Vue.createApp({
 		priceVolumeDown(index){
 			if(this.buy_products_count[index] > 0){
 				this.buy_products_count[index] = this.buy_products_count[index]-1;
-				console.log('счетчик больше 0 ='+this.buy_products_count[index])
+				
 				this.buy_products_price_basket[index] = this.buy_products_price[index]*this.buy_products_count[index];
 			}
 		},
